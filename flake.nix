b007c246d85a8dcb529356ee6503aa76bfe6218d
cmpons <cmpons@gmail.com>
2026-09-06 15:51:56 +0200

    Add Nix dev shell with Pi coding agent

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "AzerothCore development shell with the Pi coding agent";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.pi-coding-agent
+            ];
+
+            shellHook = ''
+              echo "Pi coding agent available as: pi"
+            '';
+          };
+        });
+    };
+}
